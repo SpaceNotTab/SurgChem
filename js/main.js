dbfbe7e8057b023823e2f445162553561e3135f2
basmak3r <basmak3r@gmail.com>
2024-07-21 14:43:45 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -1,37 +1,3 @@
-/* =====================================
-Template Name: 	Mediplus.
-Author Name: Naimur Rahman
-Website: http://wpthemesgrid.com/
-Description: Mediplus - Doctor HTML Template.
-Version:	1.1
-========================================*/   
-/*=======================================
-[Start Activation Code]
-=========================================
-* Sticky Header JS
-* Search JS
-* Mobile Menu JS
-* Hero Slider JS
-* Testimonial Slider JS
-* Portfolio Slider JS
-* Clients Slider JS
-* Single Portfolio Slider JS
-* Accordion JS
-* Nice Select JS
-* Date Picker JS
-* Counter Up JS
-* Checkbox JS
-* Right Bar JS
-* Video Popup JS
-* Wow JS
-* Scroll Up JS
-* Animate Scroll JS
-* Stellar JS
-* Google Maps JS
-* Preloader JS
-=========================================
-[End Activation Code]
-=========================================*/ 
 (function($) {
     "use strict";
      $(document).on('ready', function() {
